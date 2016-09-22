681a4d75b84a1a580c340413c5e062853900961b
ekponk <ian.knopke@gmail.com>
2016-09-22 23:08:41 +0100

    More project setup

---
+++ audioconvert/audioconvert.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import pprint
+
+
+######################################################################
+pp = pprint.PrettyPrinter(indent=4)
+
+
+def convert(fname):
+    """docstring for convert"""
+
+    # 
+
+
+
+def main():
+
+    pp.pprint("start")
+
+
+######################################################################
+if __name__ == '__main__':
+
+    main()
+
