9879fa48b16e0c17ddbc05693b33718c80f6a3e1
ekponk <ian.knopke@gmail.com>
2016-09-22 23:17:35 +0100

    Adding some comments as a skeleton

--- audioconvert/audioconvert.py
+++ audioconvert/audioconvert.py
@@ -9,10 +9,26 @@ import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
 
-def convert(fname):
+def convert(path,fname):
     """docstring for convert"""
 
-    # 
+    # split into directory and fname
+    # TODO assert fname and maybe dirname
+
+    # TODO what to do about different types?
+    # just flac and wav for now
+    # output format always mp3
+
+    ## copy to local and rename file
+
+    ## convert to mp3
+
+    ## rename to original with extension
+    ## copy back to original directory
+
+
+
+# TODO need a function to split paths into path/fname
 
 
 
